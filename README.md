af36e3fdf152ddd9aca4a9a9d8097c4e50a358f3
MatthiasBlc <mat.bouloc@gmail.com>
2022-04-14 15:03:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Bonjour,
+
+Ce dossier est consitué de 4 programmes ayants chacun une fonction différente.
+
+1/ liste des fichiers:
+00_journalists.rb
+01_cryptocurrencies.rb
+
+
---
+++ lib/00_journalists.rb
@@ -0,0 +1,117 @@
+$list = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+
+# 1 -Combien y a-t-il de handle dans cette array ?
+def h_handle()
+  h_list = $list.length
+  puts h_list
+end
+
+
+# 2 - Quel est le handle le plus court de cette liste ?
+def short_handle
+  short_list = $list.min_by(&:length)
+  puts short_list
+end
+
+
+# 3 - Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+def car5_handle
+  car5_list =[]
+  list_lenth = $list.length
+  list_lenth.times{ |i| car5_list.push $list[i].size-1 }
+  puts car5_list.count(5)
+  # journalists.count {|handle| handle.length == 6
+end
+
+# ///////////////////////////TO DOOOOOOOOOOOOOOOOOOOOOO///////////////////////
+# 4 - Combien commencent par une majuscule (première lettre juste après le @) ?
+def maj_handle
+  puts $list.count {|handle| handle[1] =~/[[:upper:]]/}
+end
+
+
+# 5 - Trie la liste de handle par ordre alphabétique.
+def alpha_handle
+  puts $list.sort_by{|handle| handle[1].downcase}
+  # padding = 4
+  # alpha_list = $list.sort{|a,b|
+  #   a,b = [a,b].map{|s| s.gsub(/\d+/){|m| "0"*(padding - m.size) + m } }
+  #   a<=>b
+  # }
+  # puts alpha_list
+end
+
+
+# 6 - Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+def order_length_handle
+  # order_length_list = $list.sort_by {|x| x.length}
+  order_length_list = $list.sort_by(&:length)
+  puts order_length_list
+end
+
+
+# 7 - Quelle est la position dans l'array de la personne @epenser ?
+def epenser_pose_handle
+  puts $list.index('@epenser')
+end
+
+
+# 8 - Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+def repart_handle
+  ($list.max_by(&:length).size-1).times {|i| puts "#{i+1} dans #{$list.count {|handle| handle.size-1 == (i+1)}}"}
+  # journalists.select {|handle| handle.size == 1}
+  # into boucle dans taille max taille et min_by
+end
+
+
+def system
+  while true
+    puts "Enter : \n|1 - Combien y a-t-il de handle dans cette array ? \n|2 - Quel est le handle le plus court de cette liste ? \n|3 - Combien y-a-t'il de handle contenant 5 caractères ? \n|4 - Combien commencent par une majuscule ? \n|5 - Trier la liste de handle par ordre alphabétique. \n|6 - Trier la liste de handle par taille des handle. \n|7 - Quelle est la position dans l'array de la personne @epenser ? \n|8 - Sortir une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc) \n|0 - For leave "
+    print ">"
+    choice = gets.chomp
+    case choice
+      when "1"
+        h_handle()
+        puts "\ndone\n "
+        next 
+      when "2"
+        short_handle()
+        puts "\ndone\n "
+        next 
+      when "3"
+        car5_handle()
+        puts "\ndone\n "
+        next 
+      when "4"
+        maj_handle()
+        puts "\ndone\n "
+        next 
+      when "5"
+        alpha_handle()
+        puts "\ndone\n "
+        next 
+      when "6"
+        order_length_handle()
+        puts "\ndone\n "
+        next 
+      when "7"
+        epenser_pose_handle()
+        puts "\ndone\n "
+        next 
+      when "8"
+        repart_handle()
+        puts "\ndone\n "
+        next 
+      when "0"
+        puts "\nGoodbye\n"
+        break
+      else
+        puts "\nUnknow choice\n"
+        next
+    end
+  end
+end
+
+system()
+
